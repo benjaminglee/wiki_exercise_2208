32af95b2aa30e23e8251416c509908266b818d7a
Benjamin Lee <benjaminlee0727@gmail.com>
2022-08-10 19:30:12 -0500

    msg

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,9 +1,8 @@
 let router = require('express').Router();
 const {Page, User} = require('../models');
 
-const {addPage, editPage, layout, main, userList, userPages, wikipage} = require('../views');
-
-
+const {addPage, editPage, layout, main, userList, userPages, wikiPage} = require('../views');
+  
 router.get('/', (req, res) => {
     res.send('got to GET /wiki/')
 })
@@ -22,5 +21,21 @@ router.post('/', async (req, res, next) => {
 router.get('/add', (req, res) => {
     res.send(addPage())
 })
+
+router.get('/:slug', async (req, res, next) => {
+    try{
+        const page = await Page.findOne({
+            where: {
+                slug: req.params.slug
+            }
+        });
+        res.json(page).slug;
+
+        console.log(pageobj);
+        //res.send('hi')
+        //res.send(wikiPage(page));
+    }
+    catch(error) {next (error)}
+  });
 
 module.exports = router;
